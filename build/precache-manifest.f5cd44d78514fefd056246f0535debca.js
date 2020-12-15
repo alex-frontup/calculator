5a0f455785e9849491e0359281c7a9965d2f67a7
varlamovas <akiriakira1361@gmail.com>
2020-12-15 22:38:48 +0300

    add build folder

---
+++ build/precache-manifest.f5cd44d78514fefd056246f0535debca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77fd46f34a8300749d4e95971cdba174",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "b7595b5766ccbfc62889",
+    "url": "/static/css/main.6fdcdd19.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "b7595b5766ccbfc62889",
+    "url": "/static/js/main.e647b3c8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
