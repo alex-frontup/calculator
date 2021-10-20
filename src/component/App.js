9aaa82ad9c294bd8a1dbdaaf0b81565ad790fb89
alex-frontup <81036576+alex-frontup@users.noreply.github.com>
2021-10-20 20:46:10 +0300

    Update App.js

--- src/component/App.js
+++ src/component/App.js
@@ -19,7 +19,7 @@ export default class App extends React.Component {
     return (
       <div className="component-app">
         <Display value={this.state.next || this.state.total || "0"} />
-<div>{process.env.PUK || 'defaultPUK'}</div>
+<div>{process.env.REACT_APP_PUK || 'defaultPUK'}</div>
         <ButtonPanel clickHandler={this.handleClick} />
       </div>
     );
